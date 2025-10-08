9a9306fcbf62f7da7710fb82e9f4f0c08b7d4a62
Sosenko Yaroslav <82959750+strategsc2@users.noreply.github.com>
2025-10-08 22:28:44 +0300

    Add files via upload

---
+++ task0/main.py
@@ -0,0 +1,24 @@
+import csv
+
+def main():
+    with open('task2.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',')
+        nodes = set()
+        rows = []
+        for row in spamreader:
+            nodes.update(list(map(int, row)))
+            rows.append(row)
+
+        my_matrix = [([0] * len(nodes)) for i in range(len(nodes))]
+        for row in rows:
+            left, right = list(map(int, row))
+            my_matrix[left-1][right-1] = 1
+
+    # print(f'{my_matrix}')
+    return my_matrix
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == '__main__':
+    main()
